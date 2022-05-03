18cb41a2309f3094353375d22d17e5d1f4c041e5
MarinaBartulov <bartulovmarina1@gmail.com>
2022-05-03 16:21:37 +0200

    showing my assets and creator dashboard done on frontend. Contracts deployed on Mumbai test network

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftaddress = "0x0511cfe80F41Bf2733245B17F9a3b29A96ecE469";
+export const nftmarketaddress = "0x44Cdc8f47faaB91C252b1a14f0E1DA5eB9245001";
